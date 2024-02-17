45d923483ad0b43c74e677e6475e5ca7b482f4cb
Ayesha Ahmed <ayeshaahmed1573@gmail.com>
2024-02-18 01:39:49 +0500

    update

--- app.js
+++ app.js
@@ -1,46 +1,35 @@
-const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q="
-const apikey="9774657a03c18f6617fcefb0ba55971d"
-const searchinp=document.querySelector('#input');
-const searchbtn=document.querySelector('.search button')
-const image=document.querySelector("#main")
-async function checkweather(city){
-    const resp= await fetch(apiurl+city+`&appid=${apikey}`);
-    var data=await resp.json()
-    console.log(data);
+const temp = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+const image = document.getElementById('main'); // Assuming 'main' is the ID of the image element
 
-const cit=document.querySelector('.city').innerHTML= data.name;
-const temp=document.querySelector('.temp').innerHTML=Math.round(data.main.temp)+"°C";
-const humidity=document.querySelector('.humidity').innerHTML=Math.round(data.main.humidity)+"%";
-const wind=document.querySelector('.wind').innerHTML=Math.round(data.wind.speed)+"Km/h";
-if (data.weather[0].main=="clear") {
-    image.setAttribute('src', "https://cdn-icons-png.flaticon.com/512/3222/3222691.png");
+const searchbtn = document.querySelector('button');
+const searchinp = document.getElementById('input');
+
+function checkweather(city) {
+    // Assume 'data' is already defined and contains weather data
     
-}  
-else if (data.weather[0].main=="Mist") {
-    image.setAttribute('src', "mist.png");
-    
-}
-else if (data.weather[0].main=="Clouds") {
-    image.setAttribute('src', "scatteredclouds.png");
-    
-}
-else if (data.weather[0].main=="Drizzle") {
-    image.setAttribute('src', "rain.png");
-    
-}
-else if (data.weather[0].main=="Rain") {
-    image.setAttribute('src', "showerrain.png");
-    
-}
-else if (data.weather[0].main=="Snow") {
-    image.setAttribute('src', "snow.png");
-    
-}
-document.querySelector(".weather").style.display="block"
+    temp.innerHTML = Math.round(data.main.temp) + "°C";
+    humidity.innerHTML = Math.round(data.main.humidity) + "%";
+    wind.innerHTML = Math.round(data.wind.speed) + "Km/h";
+
+    if (data.weather[0].main == "Clear") {
+        image.setAttribute('src', "https://cdn-icons-png.flaticon.com/512/3222/3222691.png");
+    } else if (data.weather[0].main == "Mist") {
+        image.setAttribute('src', "mist.png");
+    } else if (data.weather[0].main == "Clouds") {
+        image.setAttribute('src', "scatteredclouds.png");
+    } else if (data.weather[0].main == "Drizzle") {
+        image.setAttribute('src', "rain.png");
+    } else if (data.weather[0].main == "Rain") {
+        image.setAttribute('src', "showerrain.png");
+    } else if (data.weather[0].main == "Snow") {
+        image.setAttribute('src', "snow.png");
+    }
+
+    document.querySelector(".weather").style.display = "block";
 }
 
-searchbtn.addEventListener("click",()=>{
-    checkweather(searchinp.value)
-})
-
-
+searchbtn.addEventListener("click", () => {
+    checkweather(searchinp.value);
+});
